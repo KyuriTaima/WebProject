b711dee3ba1493e16dac3b00a5e3e1779008be2b
SaraMezine <sara.mezine@epfprojets-sceaux.com>
2018-03-16 00:01:10 +0100

    creation d'un fichier categorie pour gerer tout l'accueil

---
+++ SiteWebTom/categorie.php
@@ -0,0 +1,42 @@
+<?php include("Acceuil.php");
+
+if (!isset($_SESSION['login'])) {
+   header ('Location: index.php');
+   exit(); 
+} 
+?>
+<html>
+
+<head>
+    <title>My document</title>
+    <link rel="stylesheet" href="assets/style.css" />  
+
+</head>
+
+<body>
+    <?php
+    try
+    {
+        $bdd = new PDO('mysql:host=localhost;dbname=Entraide;charset=utf8', 'root', 'root',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    } 
+    catch(Exception $e)
+    {
+        die('Erreur : '.$e->getMessage());
+    }    
+
+    $rep =  $bdd->prepare('SELECT corpsTexte, login FROM articles WHERE categorie = ?');
+    $rep->execute(array($_GET['cat']));
+
+    while ($donnees= $rep->fetch())
+    {
+        $login = $donnees['login'];
+        echo '<p>'. "<a href='profile.php?login=$login'> $login " . ' -- '. htmlspecialchars($donnees['corpsTexte']) . '<br />' . '</p>';
+    }
+    $rep->closeCursor();   
+
+    ?>
+
+
+</body>
+
+</html>
