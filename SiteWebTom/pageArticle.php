85c5527abda9dd7cc5373ec8e0b8581fcfdbef59
kyuritaima <sulwen.delacroix@gmail.com>
2018-03-15 16:51:47 +0100

    page d'article ajoutée pour Maths 3eme

--- SiteWebTom/pageArticle.php
+++ SiteWebTom/pageArticle.php
@@ -1,3 +1,45 @@
-<?php
-echo $_GET['id'];
+<?php include("Acceuil.php");
+if (!isset($_SESSION['login'])) {
+   header ('Location: index.php');
+   exit(); 
+} 
 ?>
+<html>
+
+<head>
+    <title>My document</title>
+    <link rel="stylesheet" href="assets/style.css" />  
+
+</head>
+
+<body>
+    <?php
+    $id = $_GET['id'];
+    try
+    {
+        $bdd = new PDO('mysql:host=localhost;dbname=Entraide;charset=utf8', 'root', '',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    } 
+    catch(Exception $e)
+    {
+        die('Erreur : '.$e->getMessage());
+    }    
+
+    $rep =  $bdd->query("SELECT corpsTexte, login, categorie FROM articles WHERE id = '$id'");
+
+    $donnees= $rep->fetch();
+        ?>
+        <div id=article>
+        	<h1><?php echo $donnees['login']; ?></h1><br />
+        	<p>
+        		<?php echo $donnees['corpsTexte']; ?>
+        	</p>        	
+        </div>
+    <?php
+    $rep->closeCursor();   
+
+    ?>
+
+
+</body>
+
+</html>
