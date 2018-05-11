0e3885aeb5ff008b6f60b578391ca8ce778c62ce
Tom Rollet <tom.rollet@epfedu.fr>
2018-05-12 00:19:26 +0200

    + fix redirecton New article

--- Controllers/NewArticle.php
+++ Controllers/NewArticle.php
@@ -2,6 +2,10 @@
 <?php
 
 session_start();
+if (!isset($_SESSION['login'])) {
+   header ('Location: /EntraideMVC/Controllers/Connexion.php');
+   exit(); 
+} 
 $EntraideMVC_DIR = $_SERVER["DOCUMENT_ROOT"]. "/EntraideMVC";
 require ($EntraideMVC_DIR. '/Models/userManager.class.php');
     require ($EntraideMVC_DIR. '/Models/user.class.php');
